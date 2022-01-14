4bbaae18b017195925e5806a787b653851fefa47
Randy <regd25@gmail.com>
2022-01-13 23:03:16 -0600

    router working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const readline = require('readline');
 const fs = require('fs')
 const path = require('path');
-const { textToMatrix, maxRoute } = require('./utils');
+const { textToMatrix, toRoutes } = require('./utils');
 
 
 const rl = readline.createInterface({
@@ -9,23 +9,18 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 let document
-document = fs.readFileSync(path.resolve("/Users/randygala/Dropbox/Trabajo/Entrevistas/GrainChain/test-1.txt"), 'utf-8')
+document = fs.readFileSync(path.resolve("C:/Users/Randy Gala/develop/interviews/light-test/test-2.txt"), 'utf-8')
 
 // rl.question('Path: ', function (p) {
 //     document = fs.readFileSync(path.resolve("C:\Users\Randy Gala\Dropbox\Trabajo\Entrevistas\GrainChain\test-1.txt"), 'utf-8')
 // });
 
 const matrix = textToMatrix(document)
+console.log(matrix)
+let routes = toRoutes(matrix)
 
-let maxX = 0
-let maxY = 0
-for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j){
-        const result = maxRoute(matrix[j], i)
 
-    }
-}
 
-console.log(max)
+console.log(routes)
 
 
--- utils.js
+++ utils.js
@@ -1,12 +1,45 @@
 const textToMatrix = (text = '') => text.split('\r\n').map(row => row.split('').map(item => Number(item)))
 
+const replaceWithRoutes = (array, index, prev, next) => {
+    for (let i = 0; i < array.length; i++) {
+        if(i >= index - prev && i <= index + next) {
+            array[i] = 1
+        }
+    }
+    return array
+}
+
+const toRoutes = (matrix) => {
+    let result = []
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++){
+            const resultX = maxRoute(matrix[y], x)
+            const resultY = maxRoute(matrix.map( item => item[x]), y)
+            if(resultX && resultY) result.push({
+                x: {
+                    ...resultX,
+                    index: x
+                },
+                y: {
+                    ...resultY,
+                    index: y
+                },
+                total: resultX.total + resultY.total
+            })
+        }
+    }
+    result = result.sort((a, b)=> {
+        if(a.total < b.total) return 1
+        if(a.total > b.total) return -1
+        return 0
+    })
+    return result
+}
+
 const maxRoute = (array, init = 0) => {
     const next = countRightZeros(array, init)
     const prev = countLeftZeros(array, init)
-
-    if(prev === -1 && next === -1) {
-        return null
-    }
+    if (prev === -1 && next === -1) return null
     return {
         prev,
         next,
@@ -15,26 +48,29 @@ const maxRoute = (array, init = 0) => {
 }
 
 const countRightZeros = (array, index = 0) => {
-    if(!array.length) return -1
+    if (!array.length) return -1
     const founded = array.indexOf(1, index)
-    if(founded === index) return - 1
-    if(founded === - 1) return array.length - 1 - index
+    if (founded === index) return - 1
+    if (founded === - 1) return array.length - 1 - index
     return founded - index - 1
 }
 
 const countLeftZeros = (array, index = 0) => {
     array = array.reverse()
-    if(!array.length) return -1
-    const founded = array.indexOf(1, array.length - 1 - index )
-    if(founded === array.length - 1 - index) return -1
-    if(founded === -1) return index
+    if (!array.length) return -1
+    const founded = array.indexOf(1, array.length - 1 - index)
+    array = array.reverse()
+    if (founded === array.length - 1 - index) return -1
+    if (founded === -1) return index
     return founded - (array.length - index)
 }
 
 
 module.exports = {
     textToMatrix,
-    maxRoute,
     countLeftZeros,
-    countRightZeros
+    countRightZeros,
+    maxRoute,
+    toRoutes,
+    replaceWithRoutes
 }
